5552df71ff0066097c460ff5d017c9237198c144
aviverit <andrew.viverito@gmail.com>
2017-07-05 01:01:43 -0500

    v.0.0.1 "Just getting started"

---
+++ LCMBattleship/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCMBattleship
+{
+    class Solution
+    {
+        public int CountBattleships(char[,] board)
+        {
+            int count = 0;
+
+
+
+            return count;
+        }
+    }
+}
