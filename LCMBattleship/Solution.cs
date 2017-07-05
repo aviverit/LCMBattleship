46274e35d2ae38b735c25c5a522b61cb4dffc8ec
aviverit <andrew.viverito@gmail.com>
2017-07-05 02:14:35 -0500

    v.1.0.0 "Slow but successful"

--- LCMBattleship/Solution.cs
+++ LCMBattleship/Solution.cs
@@ -11,30 +11,33 @@ namespace LCMBattleship
         public int CountBattleships(char[,] board)
         {
             int count = 0;
-            int[,] tile = new int[1, board.GetLength(0) * board.GetLength(1) / 2];
-            tile[0, 0] = -1;
-            int tileX = 0;
             int k, l;
 
-            for (int i = 0; i < board.GetLength(0); i++) { //Traverse rows
-                for (int j = 0; j < board.GetLength(1); j++) { //Traverse columns
-                    if (board[i, j] == 'X') {
+            for (int i = 0; i < board.GetLength(0); i++)
+            { //Traverse rows
+                for (int j = 0; j < board.GetLength(1); j++)
+                { //Traverse columns
+                    if (board[i, j] == 'X')
+                    {
                         k = 1;
                         l = 1;
-                        while (board[i - k, j] != '.') {
+                        while (i >= k && board[i - k, j] != '.')
+                        {
                             k++;
                         }
-                        while (board[i, j - l] != '.') {
+                        while (j >= l && board[i, j - l] != '.')
+                        {
                             l++;
                         }
-                        if (k == 1 && l == 1) {
+                        if (k == 1 && l == 1)
+                        {
                             count++;
                         }
                     }
                 }
             }
 
-        return count;
+            return count;
         }
     }
 }
